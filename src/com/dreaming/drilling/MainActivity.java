696ba2ddb3dbd8aac645a0192579edc8146ebd6c
yalong-tse <yalong.tse@gmail.com>
2013-12-08 18:50:14 +0800

    refactor package path

--- src/com/dreaming/drilling/MainActivity.java
+++ src/com/dreaming/drilling/MainActivity.java
@@ -1,14 +1,20 @@
-package com.example.drilling;
+package com.dreaming.drilling;
 
+
+import com.dreaming.drilling.utils.GlobalConstants;
+import com.example.drilling.R;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.view.Menu;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
 	private String title_name = "°à±¨Â¼Èë";
+	
+	protected SharedPreferences sharedPrefs;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +33,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void buildingPreference()
+	{
+		this.sharedPrefs = this.getSharedPreferences(GlobalConstants.PREFERENCE_NAME, GlobalConstants.MODE);
+		
+		if (!this.sharedPrefs.getBoolean("first2", true)) return;
+		
+		SharedPreferences.Editor localEditor = this.sharedPrefs.edit();
+		
+		
 	}
 
 }
---
+++ src/com/dreaming/drilling/utils/GlobalConstants.java
@@ -0,0 +1,13 @@
+package com.dreaming.drilling.utils;
+
+/**
+ * drilling 所需的全局变量
+ * */
+public class GlobalConstants {
+	
+	
+	// share Preference 配置
+	public static int MODE = android.app.Activity.MODE_PRIVATE;//定义访问模式为私有模式
+	public static final String PREFERENCE_NAME = "drilling";//设置保存时的文件的名称
+
+}
