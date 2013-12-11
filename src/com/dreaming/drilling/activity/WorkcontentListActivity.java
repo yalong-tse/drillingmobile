4d566135c4a04ee1c22b7c0571343502a46772a8
yalong-tse <yalong.tse@gmail.com>
2013-12-11 21:18:18 +0800

    add workcontent listview

---
+++ src/com/dreaming/drilling/activity/WorkcontentListActivity.java
@@ -0,0 +1,43 @@
+package com.dreaming.drilling.activity;
+
+import com.dreaming.drilling.R;
+import com.dreaming.drilling.R.layout;
+import com.dreaming.drilling.R.menu;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class WorkcontentListActivity extends Activity {
+
+	private String title_name = "°à±¨ÁÐ±í";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_workcontent_list);
+		initview();
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.workcontent_list, menu);
+		return true;
+	}
+	
+	/**
+	 * 
+	 * */
+	public void initview()
+	{
+		TextView textView_title = (TextView)findViewById(R.id.title);
+		textView_title.setText(title_name);
+	}
+
+	
+	
+	
+}
